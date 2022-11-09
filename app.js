f1bbf24bd9fdcadbc8db3a7a076cef06e4c84757
Kenni Woodard <kennikristine@gmail.com>
2022-11-08 17:54:37 -0800

    two bugs appear on load page

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 /* Imports */
+import { renderBug } from './render-utils.js';
 
 /* Get DOM Elements */
+const bugListEl = document.querySelector('.bugs');
 
 /* State */
+const bugs = [
+    { id: 1, name: 'javascript', hp: 3 },
+    { id: 2, name: 'html', hp: 1 },
+];
 
 /* Events */
 
 /* Display Functions */
+
+function displayBugs() {
+    bugListEl.textContent = '';
+
+    for (let bug of bugs) {
+        const bugEl = renderBug(bug);
+        bugListEl.append(bugEl);
+    }
+}
+
+displayBugs();
 
 // (don't forget to call any display functions you want to run on page load!)
---
+++ render-utils.js
@@ -0,0 +1,23 @@
+export function renderBug(bug) {
+    const bugEl = document.createElement('div');
+    const nameEl = document.createElement('p');
+    const emojiEl = document.createElement('p');
+    const hpEl = document.createElement('p');
+
+    bugEl.classList.add('bug');
+
+    nameEl.textContent = bug.name;
+    hpEl.textContent = bug.hp < 0 ? 0 : bug.hp;
+    hpEl.id = `bug-hp-${bug.id}`;
+
+    emojiEl.id = `bug-id-${bug.id}`;
+    emojiEl.textContent = bug.hp > 0 ? '👾' : '✅';
+
+    if (bug.hp < 0) {
+        bugEl.classList.add('defeated');
+    }
+
+    bugEl.append(nameEl, emojiEl, hpEl);
+
+    return bugEl;
+}
